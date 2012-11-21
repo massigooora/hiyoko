1c3ab460631f4dc2c6143cff1f002d6bf473adcb
yamaguchi <yamaguchi@haw.co.jp>
2012-11-22 00:52:54 +0900

    init

---
+++ workspace/bookmark_service/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookmarkService::Application.config.secret_token = 'e03fc521c9294ab492b2a2760a0de1c6ecc66a77232da8462cb798da14b279e94e3a65016451fec6741108b72dcc4a77647aa799d0b56215e078f2f0b9f99ec3'
